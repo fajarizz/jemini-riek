f5da9550b640c14e1506f8a72c57e52d4ddeabd9
rafajarizz <rafafajarizz@gmail.com>
2025-09-09 20:53:35 +0700

    [ADD] Implement application sidebar component with menu items

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './components/theme-provider'
+import {ThemeProvider} from './components/theme-provider'
+import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar.tsx";
+import {AppSidebar} from "@/components/app-sidebar.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            <SidebarProvider>
+                <AppSidebar/>
+                <SidebarTrigger/>
+                <App/>
+            </SidebarProvider>
+        </ThemeProvider>
+    </StrictMode>,
 )
